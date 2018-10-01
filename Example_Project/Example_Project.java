c53f28e92ff6d8c184afa22a6206b13896561a30
EPCarter <Carter.Eri7200@stu.stech.edu>
2018-09-30 20:51:29 -0600

    Update message

---
+++ Example_Project/Example_Project.java
@@ -0,0 +1,16 @@
+/*
+Author: Erik Carter
+Email: Carter.Eri7200@stu.stech.edu
+Course: CSCI 1105 - Introduction to Programming
+Assignment: Final_Project
+Date: .  2018
+Summary: 
+*/
+import java.util.Scanner;
+class Example_Project {
+public static void main(String[] args) {
+Scanner reader = new Scanner(System.in); 
+System.out.print("Enter a number: ");
+String input = reader();        
+}
+}
